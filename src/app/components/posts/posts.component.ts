276c51f3b9cd23c5a4628146bd1cfdbdcb680436
Gabriel Rzecznik <rzecznike@gmail.com>
2025-11-30 06:04:03 -0300

    feat(posts): improve post layout and styling with responsive adjustments

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -3,10 +3,12 @@ import { Post } from '../../core/models/post/post.model';
 import { PostService } from '../../core/services/post.service';
 import { FullDateTime } from '../../pipes/full-date-time.pipe';
 import { CommonModule } from '@angular/common';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-posts',
-  imports: [ FullDateTime, CommonModule],
+  imports: [ FullDateTime, CommonModule, MatButtonToggleModule, MatIconModule],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -14,6 +16,7 @@ import { CommonModule } from '@angular/common';
 export class PostsComponent implements OnInit {
   posts: Post[] = [];
   colSize = 3;
+  public userAvatarUrl = 'https://jornada.us-sea-1.linodeobjects.com/imagenes/2024/6/24/372280_1_102812_raw.jpg';
 
   constructor(private postService: PostService, private cd: ChangeDetectorRef) {}
 
@@ -31,7 +34,7 @@ export class PostsComponent implements OnInit {
   }
 
   increaseSize() {
-    const sizes = [2, 3, 4, 6, 12];
+    const sizes = [4, 6, 12];
     const idx = sizes.indexOf(this.colSize);
     if (idx > 0) {
       this.colSize = sizes[idx - 1];
